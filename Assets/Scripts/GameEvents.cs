2d0665ec6245e43202f0d1a7636d7f104f56aec4
lawrence <lawrence.parry@gmail.com>
2020-05-13 09:29:38 +1200

    Folder Structure, Basic Scripts

---
+++ Assets/Scripts/GameEvents.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+public class GameEvents : MonoBehaviour
+{
+    private static GameEvents instance = null;
+
+    public static GameEvents Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new GameEvents();
+            }
+            return instance;
+        }
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+
+    public UnityEvent LevelLoaded;
+    public UnityEvent GameStart;
+    public UnityEvent GameEnd;
+}
