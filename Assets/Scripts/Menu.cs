867721892b9f26cb940bb459d3b61945e4665063
Josh <joshuaeric.malcolm@gmail.com>
2020-05-14 11:36:44 +1200

    Menu Scene and countdown timer prefab

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public void LoadScene(int _Level)
+    {
+        SceneManager.LoadScene(_Level);
+    }
+    public void ExitScene()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private Text timerUI;
+    private float timer;
+    public bool pause;
+    [SerializeField]
+    private float startTimerValue;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetTimer();
+        pause = false;
+        timerUI = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int timerInt = Convert.ToInt32(timer);
+        timerUI.text = "" + timerInt;
+
+        if(pause == false)
+        {
+            TimerCountDown();
+        }
+    }
+    void ResetTimer()
+    {
+        timer = startTimerValue;
+    }
+    void TimerCountDown()
+    {
+        timer -= 1 * Time.deltaTime;
+    }
+}
