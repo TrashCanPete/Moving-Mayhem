4723af1f80deb3e571bf3de28f5277b5c7ed9ffb
Josh <joshuaeric.malcolm@gmail.com>
2020-05-21 11:01:22 +1200

    Added basic destruction script

---
+++ Assets/Scripts/Destruction.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destruction : MonoBehaviour
+{
+    public GameObject piece1;
+    public GameObject piece2;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("Player colliding");
+            Instantiate(piece1, transform.position + (transform.up * 1.5f), transform.rotation);
+            Instantiate(piece2, transform.position + (transform.up * 1.5f), transform.rotation);
+            Invoke("destroy", 0.05f);
+        }
+    }
+    void destroy()
+    {
+        Destroy(this.gameObject);
+    }
+}
