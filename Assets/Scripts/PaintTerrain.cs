a5ef952c1fad0b72b136fa10a63e8b8e35afb4cd
lawrence <lawrence.parry@gmail.com>
2020-05-26 10:30:35 +1200

    Added support for multiple terrain layers

--- Assets/Scripts/PaintTerrain.cs
+++ Assets/Scripts/PaintTerrain.cs
@@ -1,18 +1,53 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using System;
 public class PaintTerrain : MonoBehaviour
 {
-    [SerializeField] LayerMask paintLayers;
     [HideInInspector] public Terrain t;
     [HideInInspector] public GameObject currentTerrain;
     [SerializeField] float paintingDist;
     const int size = 7;
-    float[,,] map = new float[size, size, 2];
-    float[] textureValues = new float[2];
+    const float threshhold = 0.75f;
+    float[,,] mainMap = new float[size, size, 2];
+    //float[] textureValues = new float[2];
 
     private void Start()
+    {
+        StartCoroutine(Check());
+    }
+    private IEnumerator Check()
+    {
+        while (enabled)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, -transform.up, out hit, paintingDist))
+            {
+                if (hit.collider.tag == "Grass")
+                {
+                    if (hit.transform.gameObject != currentTerrain)
+                    {
+                        if (hit.transform.gameObject.GetComponent<Terrain>() != null)
+                        {
+                           GetTerrain(hit.transform.gameObject);
+                        }
+                    }
+                    Paint(transform.position,mainMap);
+                }
+            }
+            yield return new WaitForSeconds(1 /60);
+        }
+    }
+    void GetTerrain(GameObject obj)
+    {
+        Debug.Log("New Terrain here");
+        t = obj.GetComponent<Terrain>();
+        float[,,] map = new float[size, size, t.terrainData.terrainLayers.Length];
+        map = SetMap(map);
+        currentTerrain = obj;
+        mainMap = map;
+    }
+    float [,,] SetMap(float[,,] map)
     {
         for (int y = 0; y < size; y++)
         {
@@ -22,44 +57,18 @@ public class PaintTerrain : MonoBehaviour
                 map[x, y, 1] = 0;
             }
         }
-        StartCoroutine(Check());
-    }
-    private IEnumerator Check()
-    {
-        while (enabled)
-        {
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, -transform.up, out hit, paintingDist, paintLayers))
-            {
-                if (hit.collider.tag == "Grass")
-                {
-                    if (hit.transform.gameObject != currentTerrain)
-                    {
-                        if (hit.transform.gameObject.GetComponent<Terrain>() != null)
-                        {
-                            Debug.Log("New Terrain here");
-                            t = hit.transform.gameObject.GetComponent<Terrain>();
-                            currentTerrain = hit.transform.gameObject;
-                        }
-                    }
-                    
-                    Paint(transform.position);
-                }
-            }
-            yield return new WaitForSeconds(1 /60);
-        }
+        return map;
     }
     
-    void Paint(Vector3 position)
+    void Paint(Vector3 position,float[,,] map)
     {
         Vector3 checkingPos = GetPos(transform.position+transform.forward*1.5f);
-        CheckTexture(checkingPos);
+        bool[] paintAndScore =CheckTexture(checkingPos);
         Vector3 pos = GetPos(position);
-        if (textureValues[0] == 0)
-        {
+        if(paintAndScore[1])
             Score.points++;
-        }
-        t.terrainData.SetAlphamaps((int)pos.x, (int)pos.z, map);
+        if (paintAndScore[0])
+            t.terrainData.SetAlphamaps((int)pos.x, (int)pos.z, map);
     }
     Vector3 GetPos(Vector3 position)
     {
@@ -72,10 +81,26 @@ public class PaintTerrain : MonoBehaviour
         pos.z = Mathf.Clamp(pos.z, 0, t.terrainData.alphamapHeight-size);
         return pos;
     }
-    void CheckTexture(Vector3 position)
+    bool[] CheckTexture(Vector3 position)
     {
         float[,,] aMap = t.terrainData.GetAlphamaps((int)position.x, (int)position.z, 1, 1);
-        textureValues[0] = aMap[0, 0, 0];
-        textureValues[1] = aMap[0, 0, 1];
+        float[] textureValues = new float[aMap.GetLength(2)];
+        for (int i=0;  i < aMap.GetLength(2); i++)
+        {
+            textureValues[i] = aMap[0, 0, i];
+
+        }
+        bool paint = false;
+        bool score = false;
+        if (textureValues[1] >threshhold)
+        {
+            score = true;
+        }
+        if(textureValues[1] > threshhold|| textureValues[0] > threshhold)
+        {
+            paint = true;
+        }
+        bool[] paintAndScore = new bool[] { paint, score };
+        return paintAndScore;
     }
 }
