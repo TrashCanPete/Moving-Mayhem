3934098a9ac44495f3ec0d7581280c8990337e6f
Josh <joshuaeric.malcolm@gmail.com>
2020-05-25 14:16:24 +1200

    beginnings of a highscore system

---
+++ Assets/Scripts/Highscores.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Highscores : MonoBehaviour
+{
+    const string privateCode = "zGZbIfsJfEaRT1df-mGrbARJdRVhGMcUmIfNYuFoo2jw";
+    const string publicCode = "5ecb22ad377dce0a1433afde";
+    const string webURL = "http://dreamlo.com/lb/";
+
+    public Highscore[] highscoresList;
+
+    private void Awake()
+    {
+        AddNewHighscore("Josh", 50);
+        AddNewHighscore("Finn", 85);
+        AddNewHighscore("Pete", 92);
+
+        DownloadHighscores();
+    }
+    public void AddNewHighscore(string username, int score)
+    {
+        StartCoroutine(UploadNewHighscore(username, score));
+    }
+    IEnumerator UploadNewHighscore(string username, int score)
+    {
+        UnityWebRequest www = new UnityWebRequest(webURL + privateCode + "/add/" + UnityWebRequest.EscapeURL(username) + "/" + score);
+        yield return www.SendWebRequest();
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+            print("upload successful");
+        }
+        else
+        {
+            print("Error uploading: " + www.error);
+        }
+    }
+
+    public void DownloadHighscores()
+    {
+        StartCoroutine("DownloadHighscoresFromDatabase");
+    }
+
+    IEnumerator DownloadHighscoresFromDatabase()
+    {
+        UnityWebRequest www = new UnityWebRequest(webURL + publicCode + "/pipe/");
+        www.downloadHandler = new DownloadHandlerBuffer();
+        yield return www.SendWebRequest();
+
+
+        if (string.IsNullOrEmpty(www.error))
+        {
+            FormatHighscores(www.downloadHandler.text);
+        }
+        else
+        {
+            print("Error downloading: " + www.error);
+        }
+    }
+
+    void FormatHighscores(string textStream)
+    {
+        string[] entries = textStream.Split(new char[] { '\n' }, System.StringSplitOptions.RemoveEmptyEntries);
+        highscoresList = new Highscore[entries.Length];
+        for (int i = 0; i < entries.Length; i++)
+        {
+            string[] entryInfo = entries[i].Split(new char[] { '|' });
+            string username = entryInfo[0];//getting the name data in slot 0
+            int score = int.Parse(entryInfo[1]);//getting score as a string and converting it into a int
+            highscoresList[i] = new Highscore(username, score);
+            print(highscoresList[i].username + ": " + highscoresList[i].score);
+        }
+    }
+}
+
+public struct Highscore 
+{
+    public string username;
+    public int score;
+
+    public Highscore(string _username, int _score)
+    {
+        username = _username;
+        score = _score;
+    }
+}
+
+
