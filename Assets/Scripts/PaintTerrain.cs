6b362ab839f050e8f1adeba309ec35475fe90826
lawrence <lawrence.parry@gmail.com>
2020-05-28 11:23:39 +1200

    Fixed Paint Terrain

--- Assets/Scripts/PaintTerrain.cs
+++ Assets/Scripts/PaintTerrain.cs
@@ -7,9 +7,12 @@ public class PaintTerrain : MonoBehaviour
     [HideInInspector] public Terrain t;
     [HideInInspector] public GameObject currentTerrain;
     [SerializeField] float paintingDist;
-    const int size = 7;
+    public float size = 7;
+    const float mapdiv = 1024;
     const float threshhold = 0.75f;
-    float[,,] mainMap = new float[size, size, 2];
+    float[,,] mainMap = new float[1, 1, 2];
+    int width;
+    int height;
     //float[] textureValues = new float[2];
 
     private void Start()
@@ -38,20 +41,35 @@ public class PaintTerrain : MonoBehaviour
             yield return new WaitForSeconds(1 /60);
         }
     }
+    private void OnValidate()
+    {
+        if (t != null)
+        {
+            width = (int)(size * t.terrainData.alphamapWidth / t.terrainData.size.x);
+            height = (int)(size * t.terrainData.alphamapHeight / t.terrainData.size.x);
+            float[,,] map = new float[width, height, t.terrainData.terrainLayers.Length];
+            map = SetMap(map);
+            mainMap = map;
+        }
+    }
     void GetTerrain(GameObject obj)
     {
         Debug.Log("New Terrain here");
         t = obj.GetComponent<Terrain>();
-        float[,,] map = new float[size, size, t.terrainData.terrainLayers.Length];
+        width = (int)(size * t.terrainData.alphamapWidth / t.terrainData.size.x);
+        height =(int)(size * t.terrainData.alphamapHeight / t.terrainData.size.x);
+        //int width = 7;
+        //int height = 7;
+        float[,,] map = new float[width, height, t.terrainData.terrainLayers.Length];
         map = SetMap(map);
         currentTerrain = obj;
         mainMap = map;
     }
     float [,,] SetMap(float[,,] map)
     {
-        for (int y = 0; y < size; y++)
+        for (int y = 0; y < height; y++)
         {
-            for (int x = 0; x < size; x++)
+            for (int x = 0; x < width; x++)
             {
                 map[x, y, 0] = 1;
                 map[x, y, 1] = 0;
@@ -72,13 +90,14 @@ public class PaintTerrain : MonoBehaviour
     }
     Vector3 GetPos(Vector3 position)
     {
-        Vector3 pos = (position+new Vector3(-0.5f,0,-0.5f)) - t.GetPosition();
+        Vector3 plus = new Vector3(size/2, 0, size/2);
+        Vector3 pos = (position-plus) - t.GetPosition();
         pos.x = pos.x / t.terrainData.size.x;
         pos.z = pos.z / t.terrainData.size.z;
         pos.x = pos.x * t.terrainData.alphamapWidth;
         pos.z = pos.z * t.terrainData.alphamapHeight;
-        pos.x = Mathf.Clamp(pos.x, 0, t.terrainData.alphamapWidth-size);
-        pos.z = Mathf.Clamp(pos.z, 0, t.terrainData.alphamapHeight-size);
+        pos.x = Mathf.Clamp(pos.x, 0, t.terrainData.alphamapWidth-width);
+        pos.z = Mathf.Clamp(pos.z, 0, t.terrainData.alphamapHeight-height);
         return pos;
     }
     bool[] CheckTexture(Vector3 position)
