fa3a537ef36437071bb9d676b05d93682eca3c4c
lawrence <lawrence.parry@gmail.com>
2020-06-04 15:15:08 +1200

    Pause Menu

---
+++ Assets/Pause.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    public static bool isPaused = false;
+    public GameObject pauseObj;
+    private void Update()
+    {
+        if (Input.GetButtonDown("Pause"))
+        {
+            isPaused = !isPaused;
+            pauseObj.SetActive(isPaused);
+            Time.timeScale = isPaused ? 0 : 1;
+        }
+    }
+}
