1b8095fa46b22fb40c78c5741ddb017a8c3d780e
lawrence <lawrence.parry@gmail.com>
2020-06-04 15:21:04 +1200

    Re positioned Score. Fixed Typo.

--- Assets/Scripts/ActivatePlayer.cs
+++ Assets/Scripts/ActivatePlayer.cs
@@ -13,4 +13,17 @@ public class ActivatePlayer : MonoBehaviour
     {
         playerRB.isKinematic = false;
     }
+    public void SpeedBoost(float force)
+    {
+        StartCoroutine(ApplyForce(force, 0.3f));
+    }
+    IEnumerator ApplyForce(float force,float time)
+    {
+        while (time > 0)
+        {
+            playerRB.AddForce(playerRB.transform.forward * force,ForceMode.Force);
+            yield return new WaitForFixedUpdate();
+            time -= Time.fixedDeltaTime;
+        }
+    }
 }
