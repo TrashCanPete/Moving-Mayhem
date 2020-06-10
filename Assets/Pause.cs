bd8dbc884aaaa5a426df38ea58e3db63f0f384c8
Josh <joshuaeric.malcolm@gmail.com>
2020-06-10 15:40:14 +1200

    Pause Menu

--- Assets/Pause.cs
+++ Assets/Pause.cs
@@ -8,11 +8,27 @@ public class Pause : MonoBehaviour
     public GameObject pauseObj;
     private void Update()
     {
-        if (Input.GetButtonDown("Pause"))
+        if (Input.GetButtonDown("Cancel"))
+        {
+            isPaused = !isPaused;
+            if (isPaused == true)
+            {
+                Time.timeScale = 0;
+                pauseObj.SetActive(true);
+            }
+            else if (isPaused == false)
+            {
+                Time.timeScale = 1;
+                pauseObj.SetActive(false);
+            }
+        }
+        /*if (Input.GetButtonDown("Pause"))
         {
             isPaused = !isPaused;
             pauseObj.SetActive(isPaused);
             Time.timeScale = isPaused ? 0 : 1;
-        }
+        }*/
+
+
     }
 }
